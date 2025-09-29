9619de8e379edcaf54547177dc54a1d45e3092fb
carlos <mrlrndez@gmail.com>
2025-09-29 14:52:48 -0600

    Add files via upload

---
+++ practicados/ejerciciosPracticaDos.js
@@ -0,0 +1,21 @@
+//ejercicio uno
+const texto = "Bienvenidos a la practica #2";
+
+console. log(".:: Ejercicio Uno ::.");
+console.log(texto.toLowerCase());
+console.log(texto. toUpperCase());
+
+//Ejercicio Dos
+console. log("/n\n .:: Ejercicio Dos ::.")
+const frase = "       Hola mundo;
+const fraseLimpia = frase.trim();
+console.log(fraseLimpia);
+
+//Ejercicio tres
+console.log("\n\n .:: Ejercicio Tres ::.")
+const palabra = "Hotwheels"
+const palabraNueva = palabra.substring(3, 9) I
+console.log(palabraNueva)
+
+// Ejercicio de tarea:
+// Contar los caracteres de una cadena(" Su nombre ") sin contar los espacio (Hacer uso de .trim() y .replace())
