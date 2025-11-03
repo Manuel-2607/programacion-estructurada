7bae3351f1809a13f75a3c6a056c05381f386b35
carlos <mrlrndez@gmail.com>
2025-11-02 21:48:19 -0600

    Add files via upload

---
+++ Function/calular.js
@@ -0,0 +1,22 @@
+// Ejercicios con funciones de flecha
+
+// Calcular el cuadrado de un número
+const cuadrado = (num) => {
+    return num * num;
+}
+
+//  Calcular el promedio de 3 números
+const promedio = (a, b, c) => {
+    return (a + b + c) / 3;
+}
+
+// Calcular el área de un triángulo
+const areaTriangulo = (base, altura) => {
+    return (base * altura) / 2;
+}
+
+// console.log para mostrar los resultados
+console.log("Resultados:");
+console.log("El cuadrado de 6 es:", cuadrado(6));
+console.log("El promedio de 4, 7 y 9 es:", promedio(4, 7, 9));
+console.log("El área del triángulo con base 8 y altura 5 es:", areaTriangulo(8, 5));
